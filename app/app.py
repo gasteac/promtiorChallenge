c2bd1787d846fbacf61f151a5d70da71a1118a49
Gaston Acosta <gasteac@gmail.com>
2024-08-14 22:26:03 -0300

    improve chatbot answers

--- app/app.py
+++ app/app.py
@@ -38,29 +38,28 @@ TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2")
 LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
 
-# URLs de Promtior o que contienen información relevante. 
+# URLs de Promtior o que contienen información relevante.
 urls = [
     "https://www.promtior.ai",
     "https://infonegocios.biz/plus/con-tan-solo-un-ano-promtior-esta-bionizando-las-empresas-a-traves-de-la-adopcion-de-ia-generativa-y-se-expande-a-miami",
     "https://cie.ort.edu.uy/emprendimientos/promptior",
-    "https://www.promtior.ai/service",
-    "https://www.promtior.ai/contacto",
-    "https://www.promtior.ai/use-cases",
-    "https://www.linkedin.com/company/promtior-ai/about/"
+    "https://www.forbesuruguay.com/inteligencia-artificial/firma-uruguaya-asesora-empresas-ia-fue-elegida-100-startups-potenciarse-miami-n53538",
+    "https://www.linkedin.com/company/promtior-ai/about/",
 ]
 
-# Esta funcion carga los documentos de las URLs y PDF's, los vuelve chunks, los transforma en vectores (valor semántico) y los indexa en una tienda de vectores (FAISS)
+################## Esta función carga los documentos de las URLs y PDF's, los vuelve chunks, los transforma en vectores (valor semántico) y los indexa en una tienda de vectores (FAISS)
 # Embedding es una representación de un texto en un espacio vectorial, donde cada palabra o frase es un vector....
+
 
 def get_vectorStore_from_sources(urls):
     # Defino mi text splitter para transformar los documentos en chunks
     text_splitter = RecursiveCharacterTextSplitter()
-    
+
     # Carga de paginas web
     web_loader = WebBaseLoader(urls)
     web_documents_chunks = web_loader.load_and_split(text_splitter)
 
-    # Cargar de PDF's 
+    # Cargar de PDF's
     pdf_loader = PyMuPDFLoader(
         "https://drive.google.com/uc?export=download&id=1-bHSg1UMDRqZTTGXEYpe2brPrLOeutqJ"
     )
@@ -70,13 +69,11 @@ def get_vectorStore_from_sources(urls):
     chunks = pdf_documents_chunks + web_documents_chunks
 
     # Creamos la base de conocimiento con los vectores, OpenAIEmbeddings los transforma en vectores y luego se indexan
-    vector_store = FAISS.from_documents(
-        chunks, OpenAIEmbeddings(api_key=OPENAI_API_KEY)
-    )
+    vector_store = FAISS.from_documents(chunks, OpenAIEmbeddings())
     return vector_store
 
 
-# Configuración de la app con Streamlit
+################## Configuración de la app con Streamlit
 st.set_page_config(
     page_title="PromtChall",
     page_icon=":parrot:",
@@ -97,11 +94,12 @@ if "vector_store" not in st.session_state:
     with st.spinner("Please wait a moment, I'm loading a lot of documents for you 📚"):
         st.session_state.vector_store = get_vectorStore_from_sources(urls)
 
-# Herramientas para el agente, retriever y Tavily
+
 # Retriever es una herramienta que busca documentos similares a un input en la bdc, ambos ya transformados a vectores.
 retriever = st.session_state.vector_store.as_retriever()
 
-# Creamos el agente con las herramientas que le vamos a dar
+
+################## Creamos el agente con las herramientas que le vamos a dar
 def create_agent():
     # Le doy al agente una herramienta de búsqueda para encontrar información sobre Promtior en la bdc.
     retriever_tool = create_retriever_tool(
@@ -114,10 +112,10 @@ def create_agent():
     # Defino las herramientas que le voy a dar a mi agente.
     tools = [retriever_tool, search]
 
-    # Configurar el agente, le damos un modelo y un prompt para responder preguntas.
+    ################## Configurar el agente, le damos un modelo y un prompt para responder preguntas.
 
     # Utilizamos el LLM de OpenAI
-    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0, openai_api_key=OPENAI_API_KEY)
+    llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 
     # Prompt predefinido de OpenAI, sirve pero podemos hacer uno personalizado.
     # agent_prompt = hub.pull("hwchase17/openai-functions-agent")
@@ -131,11 +129,11 @@ def create_agent():
             ),
             (
                 "system",
-                "Your answers must be long, clear and detailed.",
+                "Your answers must be long and detailed.",
             ),
             (
                 "system",
-                "You must only answer questions that are directly related to Promtior.",
+                "You must only answer questions that are related to Promtior, they founders, personal, services, etc.",
             ),
             (
                 "system",
@@ -148,7 +146,7 @@ def create_agent():
                 "system",
                 "Use the below information to decide your next action, and then respond appropriately.",
             ),
-            # TODO agent_scratchpad Creo que se utiliza para darle información sobre las herramientas que le proporcionamos y asi pueda utilizarlas. 
+            # TODO agent_scratchpad Creo que se utiliza para darle información sobre las herramientas que le proporcionamos y asi pueda utilizarlas.
             # Según la def original "Intermediate agent actions and tool output messages, will be passed in here."
             MessagesPlaceholder(variable_name="agent_scratchpad"),
         ]
@@ -157,14 +155,17 @@ def create_agent():
     # Crea un agente que utiliza el LLM de OpenAI para hacer llamadas a las funciones
     # Le pasamos un LLM que va a actuar como agente, las herramientas que le damos para que pueda buscar información y el prompt que le dice como responder
     agent = create_openai_functions_agent(llm, tools, agent_prompt)
-    # TODO no entiendo quien es el agente al final el de arriba o el de abajo o los dos al mismo tiempo? 
+    # TODO no entiendo quien es el agente al final el de arriba o el de abajo o los dos al mismo tiempo?
     # Agente que usa las herramientas, le pasamos el agente, las herramientas y verbose=False para que no muestre mensajes de debug
-    return AgentExecutor(name='Raul', agent=agent, tools=tools, verbose=False)
+    return AgentExecutor(name="Raul", agent=agent, tools=tools, verbose=False)
 
+
+# Si no existe un agente en la sesión de Streamlit, lo creamos
 if "agent_executor" not in st.session_state:
     st.session_state.agent_executor = create_agent()
 
-# Función que le pasa la pregunta del usuario y el historial de chat al agente y retorna la respuesta del agente
+
+################## Función que le pasa la pregunta del usuario y el historial de chat al agente y retorna la respuesta del agente
 def get_response(user_input):
     response = st.session_state.agent_executor.invoke(
         {"input": user_input, "chat_history": st.session_state.chat_history}
@@ -172,7 +173,7 @@ def get_response(user_input):
     return response["output"]
 
 
-# Interfaz del chatbot con Streamlit
+################## Interfaz del chatbot con Streamlit
 
 # Entrada de texto para que el usuario pueda hacer preguntas, la pregunta se guarda en user_query
 user_query = st.chat_input("Ask your question here")
@@ -184,18 +185,10 @@ if user_query:
         [HumanMessage(content=user_query), AIMessage(content=response)]
     )
 
+################## Mostramos la conversación en pantalla
 
+# AIMessage es una clase que representa los mensajes generados por la IA en la conversación
+# HumanMessage es la clase que representa los mensajes enviados por el usuario
 for message in st.session_state.chat_history:
     with st.chat_message("AI" if isinstance(message, AIMessage) else "You"):
         st.write(message.content)
-
-# # Mostramos la conversación en pantalla
-# for message in st.session_state.chat_history:
-#     # AIMessage es una clase que representa los mensajes generados por la IA en la conversación
-#     if isinstance(message, AIMessage):
-#         with st.chat_message("AI"):
-#             st.write(message.content)
-#     # HumanMessage es la clase que representa los mensajes enviados por el usuario
-#     elif isinstance(message, HumanMessage):
-#         with st.chat_message("G"):
-#             st.write(message.content)
